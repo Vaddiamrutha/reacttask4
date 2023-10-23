bbad993c6517df8cb1d549f874928b42c83f051c
VaddiAmrutha <vaddiamruthabhaskar@gmail.com>
2023-10-23 23:36:21 +0530

    first commit

--- src/UserTable.js
+++ src/UserTable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './UserTable.css';
 function UserTable() {
   const [users, setUsers] = useState([]);
